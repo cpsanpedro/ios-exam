5c32235b879756f2927f987751208952f157d095
Christiann Carlos San Pedro <cpsanpedro@yahoo.com>
2018-02-14 00:42:07 +0800

    Save to coredata and displat to list

--- cybilltek_sanpedro_christiann/cybilltek_sanpedro_christiann/Controller/ListViewController.swift
+++ cybilltek_sanpedro_christiann/cybilltek_sanpedro_christiann/Controller/ListViewController.swift
@@ -8,25 +8,32 @@
 
 import UIKit
 //import FileUtil
+
 import Alamofire
 
 class ListViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
   
+  var arrPersons: [Persons] = []
+  
   //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+      
       self.getJson()
-        // Do any additional setup after loading the view.
+      arrPersons = self.read()
+      // Do any additional setup after loading the view.
     }
   
   //MARK: TableView DataSource
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
+    return arrPersons.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier:cellIdentifiers.listCellIdentifier, for: indexPath)
-    cell.textLabel?.text = "hooray"
+    let person = arrPersons[indexPath.row]
+    
+    cell.textLabel?.text = person.firstname
     return cell
   }
   
@@ -47,6 +54,7 @@ class ListViewController: UIViewController,UITableViewDataSource,UITableViewDele
             if let json = response.result.value {
               print("json:\(json)")
               self.save(json)
+//              self.read()
           }
           case .failure(let error):
             print(error)
@@ -56,7 +64,25 @@ class ListViewController: UIViewController,UITableViewDataSource,UITableViewDele
   }
   
   func save(_ json:Any) {
+    let person = json as! [String:Any]
+    if let arrayPerson = person["person"] as? [Any] {
+      for obj in arrayPerson {
+        if let dictObj = obj as? [String:Any] {
+          Person.addPerson(withData: dictObj)
+        }
+        
+      }
+    }
+ 
+
+//    Person.addPerson(withData: formattedJson)
+  }
+  
+  func read() -> [Persons] {
+    let read = Person.fetchAllPerson()
+    print("fetch:\(read)")
     
+    return read as! [Persons]
   }
     /*
     // MARK: - Navigation
---
+++ cybilltek_sanpedro_christiann/cybilltek_sanpedro_christiann/Model/Person.swift
@@ -0,0 +1,63 @@
+//
+//  Person.swift
+//  cybilltek_sanpedro_christiann
+//
+//  Created by Christiann Carlos San Pedro on 13/02/2018.
+//  Copyright © 2018 Christiann Carlos San Pedro. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Person: NSManagedObject {
+  
+  class func addPerson(withData data:[String:Any]) {
+    let containerContext: NSManagedObjectContext = ((UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext)!
+    var result: [Persons] = []
+    var person: Persons?
+    
+    let request: NSFetchRequest<NSFetchRequestResult> = Persons.fetchRequest()
+    request.predicate = NSPredicate(format: "email = %@",data["email"] as! String)
+    
+    do {
+      result = try containerContext.fetch(request) as! [Persons]
+      if result.count == 0 {
+        NSEntityDescription.insertNewObject(forEntityName: "Persons", into: containerContext)
+        person = Persons(context:containerContext)
+      }
+      else {
+        person = result.last
+      }
+    } catch let error as NSError {
+      print("error:\(error)")
+    }
+    
+    person?.email = data["email"] as? String
+    person?.firstname = data["firstname"] as? String
+    person?.lastname = data["lastname"] as? String
+    
+    do {
+      try containerContext.save()
+    } catch let error as NSError {
+      print("error saving: \(error)")
+    }
+    
+  }
+  
+  class func fetchAllPerson() -> [NSManagedObject] {
+    let containerContext: NSManagedObjectContext = ((UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext)!
+    var result: [Persons] = []
+    
+    let request: NSFetchRequest<NSFetchRequestResult> = Persons.fetchRequest()
+    request.predicate = NSPredicate(format: "email != %@", "")
+    
+    do {
+      result = try containerContext.fetch(request) as! [Persons]
+      print("all person:\(result)")
+    } catch let error as NSError {
+      print("error fetch:\(error)")
+    }
+    return result
+  }
+
+}
